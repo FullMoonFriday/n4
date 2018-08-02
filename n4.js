6fa5093432ab6013f88c6e2179f3bf37fcb5af03
Leigh Gregg <leighgregg@pobpob.com>
2018-08-02 10:12:57 +0200

    first commit

---
+++ n4.js
@@ -0,0 +1,21 @@
+//create diagrams with d3.js
+
+//able to create lines
+
+	//able to drag lines
+
+	//able to add arrows
+
+//able to create shapes
+
+	//able to select shape
+
+	//able to drag shapes
+
+
+//able to add text to shapes
+
+//able to join shapes with lines
+
+//able to select a force
+
